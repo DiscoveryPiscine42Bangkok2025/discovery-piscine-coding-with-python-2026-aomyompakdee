ff01acc841ab708d6132282d2cda10df4ed8342a
aomyompakdee <aaomaaom.5555@gmail.com>
2026-02-10 21:40:32 +0700

    Add simple name and age example scripts
    Add two new example scripts: cell01/ex01/name.py concatenates first and last name and prints the full name; cell01/ex02/age.py sums two age variables and prints the result. These are basic exercises demonstrating string concatenation and numeric addition.

---
+++ cell01/ex01/name.py
@@ -0,0 +1,6 @@
+first_name = "ben"
+last_name = "yom"
+
+whole_name = first_name + " " + last_name
+
+print(whole_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,6 @@
+your_age = 21
+future_age = 42
+
+my_age = your_age + future_age
+
+print(my_age)
