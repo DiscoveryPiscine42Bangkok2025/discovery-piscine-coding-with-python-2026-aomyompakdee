3038bbf89ecf8e30c6a419e1c602cfee036a4083
aomyompakdee <aaomaaom.5555@gmail.com>
2026-02-11 13:57:21 +0700

    Add cell02 exercises and normalize shebang
    Add two new scripts in cell02: iszero.py (prints whether an input integer equals zero) and isneg.py (prints whether an input integer is negative, positive, or zero). Both read from stdin and convert the input to int before checking. Also update the shebang in cell01/ex01/name.py from '#!/usr/bin/env python3' to '#!/usr/bin/env python'.

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 first_name = "ben"
 last_name = "yom"
---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+num = input("input num: ")
+if int(num) == 0 :
+     print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
+
